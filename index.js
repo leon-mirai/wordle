35cf48d5cedd43cf24a9d77f1fb259ad82c0f319
Leon <leon.lee4230@gmail.com>
2025-03-20 20:24:43 +1000

    wordle valid word, backspace

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const ANS_LENGTH = 5;
 const letters = document.querySelectorAll(".scoreboard-letter");
+const WORD_URL = "https://words.dev-apis.com/word-of-the-day";
+
+async function getWord() {
+  const res = await fetch(WORD_URL);
+  const obj = await res.json();
+  return obj;
+}
+
+getWord();
 
 async function init() {
   let currentGuess = "";
@@ -15,15 +24,42 @@ async function init() {
     letters[ANS_LENGTH * rowGuess + currentGuess.length - 1].innerText = letter;
   }
 
+  function backspace() {
+    if (currentGuess.length > 0) {
+      letters[ANS_LENGTH * rowGuess + currentGuess.length - 1].innerText = "";
+      currentGuess = currentGuess.substring(0, currentGuess.length - 1);
+    }
+  }
+
   function commit() {
+    async function validateWord() {
+      const response = await fetch(request);
+      const data = await response.json();
+      console.log(data.validWord);
+      return data.validWord;
+    }
+
+    const request = new Request("https://words.dev-apis.com/validate-word", {
+      method: "POST",
+      body: JSON.stringify({ word: currentGuess }),
+    });
+
+    let isValid = validateWord();
+    console.log("isValid: ", isValid);
+
     if (currentGuess.length !== ANS_LENGTH) {
       // do nothing
       console.log("Hi");
       return;
     }
-    if (rowGuess < ANS_LENGTH) {
-      currentGuess = "";
-      rowGuess++;
+    if (isValid) {
+      if (rowGuess < ANS_LENGTH) {
+        currentGuess = "";
+        rowGuess++;
+      }
+    } else {
+      return;
+      //do nothing
     }
 
     // validate word
